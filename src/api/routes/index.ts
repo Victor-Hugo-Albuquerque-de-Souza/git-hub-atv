cbfe9f7683f321316ddcde9aa844e7a3d4e470c6
Igor Souza <igorsouzaatb@hotmail.com>
2022-07-21 11:46:43 -0300

    adicionando employees

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -1,11 +1,5 @@
-import payments from "./PaymentsRoute";
-import orders from "./OrdersRoute";
-import products from "./ProductsRoute";
 import customers from "./CustomersRoute";
-import productLines from "./ProductLinesRoute";
 import employees from "./EmployeesRoute";
-import offices from "./OfficesRoute";
-import orderDetail from './OrderDetailsRoute'
 import { Router } from "express";
 import authRouter from './AuthRoute';
 import { ensureIsAuthenticated } from "../middlewares/AuthMiddleware";
@@ -16,19 +10,7 @@ routes.use("/auth", authRouter);
 
 // routes.use(ensureIsAuthenticated);
 
-routes.use('/payments', payments);
-
-routes.use('/orders', orders);
-
-routes.use('/products', products);
-
 routes.use('/customers', customers);
-
-routes.use('/productlines', productLines);
-
 routes.use('/employees', employees);
 
-routes.use('/orderDetails', orderDetail);
-
-routes.use('/offices', offices);
 export default routes;
--- src/database/initdb.ts
+++ src/database/initdb.ts
@@ -1,10 +1,12 @@
 import { userInfo } from "os";
 import Customer from "../api/models/CustomersModels";
+import Employees from "../api/models/EmployeesModel";
 
 export const initdb = async () => {
     console.log('Aguarde sincronização de tabelas..');
     Promise.all([
-        await Customer.sync({alter:true})
+        await Customer.sync({alter:true}),
+        await Employees.sync({alter:true})
     ]).then(()=>{
         console.log('Sincronização de tabelas concluída com sucesso.')
     });
